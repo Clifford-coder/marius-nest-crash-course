51b92b04ccca353bb0a8ce98f73dc76938ba82b7
Clifford-coder <cliffowusu2727@gmail.com>
2022-02-15 01:33:53 +0000

    Added swagger docs setup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './entities/user.entities';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   // inject the user service dependency here
