5fa0fd027638c6ac892d6acb3e02f38d4372c3e0
Clifford-coder <cliffowusu2727@gmail.com>
2022-02-15 01:50:49 +0000

    Updated the user module swagger docs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './entities/user.entities';
 import { UsersService } from './users.service';
@@ -9,16 +9,20 @@ import { UsersService } from './users.service';
 export class UsersController {
   // inject the user service dependency here
   constructor(private usersService: UsersService) {}
+
+  @ApiOkResponse({ type: User, isArray: true })
   @Get()
   getUser(): User[] {
     return this.usersService.findAllUser();
   }
 
+  @ApiOkResponse({ type: User })
   @Get(':id')
   getUserById(@Param('id') id: string): User {
     return this.usersService.findUserById(id);
   }
 
+  @ApiCreatedResponse({ type: User })
   @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
