c474e00874cf1c8cf13669b4fe7b345d47d3bf91
Clifford-coder <cliffowusu2727@gmail.com>
2022-02-15 14:29:00 +0000

    Added validation pipes for users modules

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsAlpha, IsNumber } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  @IsAlpha()
   name: string;
 
   @ApiProperty({ required: false })
+  @IsNumber()
   age: number;
 }
